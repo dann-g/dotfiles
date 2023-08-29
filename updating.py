cc57cdf4074de57828e9cf531796bf6a7b9cccc4
dann-g <dn_ng@tutanota.com>
2023-08-28 21:40:44 -0500

    adding new intent

---
+++ updating.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+
+#New intent a script that:
+# 1. uses watchdog to observe file changes and modifications
+# 2. and updates any links needed
+# 3. stages changes, commit's them, and pushes them upstream
