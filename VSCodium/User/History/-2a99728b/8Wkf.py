9a6d7241f78e54281cf0d6ed9a81f3463c4641e7
dann-g <dn_ng@tutanota.com>
2023-08-28 20:24:35 -0500

    removing uneccesary organization

--- VSCodium/User/History/-2a99728b/8Wkf.py
+++
@@ -1,9 +0,0 @@
-import cv2 as cv
-import numpy as np
-
-filepath_img_dir= "/home/dmmt/Software_Devlopment/Internal/dolphi/Wallpaper"
-## read_imgs(filepathtoimage): read images to an empty np array
-    
-if __name__ == '__main__':
-    print("Initiallizing Doplhi Color Engine")
-    img = cv.imread(r, filename+filepath_img_dir)
