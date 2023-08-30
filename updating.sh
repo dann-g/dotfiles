c2191c19fd2345ffb32dff5d2173a14c3ac9a537
dann-g <dn_ng@tutanota.com>
2023-08-29 20:15:01 -0500

    updating back to what i shouldve done

---
+++ updating.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#Author: dann-g
+
+set -e
+CONFIG_FILES="$HOME/.config"
+
+# Purpose: For monitoring my dotfiles and updating this current repo.
+# Goals: 
+# 1. first goal setup watchdog
