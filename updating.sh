22bb3db779a774922688d3b0eb8d8213f9c19ad0
dann-g <dn_ng@tutanota.com>
2023-09-04 21:19:48 -0500

    properly making the script

--- updating.sh
+++ updating.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-check_files() {
-CONFIG_DIR="$HOME/.config"
-
+CONFIG_DIR="$HOME/.config" 
 DOT_NAMES=(${(f)"$(find ${CONFIG_DIR} -maxdepth 1)"})
 
-echo "${DOT_NAMES}"
-}
+###check_files##
+###function to iterate through DOT_NAMES and  
+check_files(){
 
+}
 check_files
 
 
-# Purpose: For monitoring my dotfiles and updating this current repo.
-# Goals 
-# first goal setup watchdog
+#### Purpose: For monitoring my dotfiles and updating this current repo.
+#### Goals 
+#### first goal setup watchdog
