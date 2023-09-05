f11cb6422f5235b50082243b9f3de9301349785d
dann-g <dn_ng@tutanota.com>
2023-09-04 20:25:55 -0500

    updating more up scripts

--- updating.sh
+++ updating.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-#Author: dann-g
 
-set -e
-CONFIG_FILES="$HOME/.config"
 
+CONFIG_DIR="$HOME/.config"
+
+readarray -t DOTFILE_NAMES  < <(find "${CONFIG_DIR}" -maxdepth 1)
+echo "${DOTFILE_NAMES}"
 # Purpose: For monitoring my dotfiles and updating this current repo.
-# Goals: 
-# 1. first goal setup watchdog
+# Goals 
+# first goal setup watchdog
